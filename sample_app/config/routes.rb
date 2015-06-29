185250c33a4952419fc5a7e52937b1897239eada
wardch <charlieward18@gmail.com>
2015-06-29 12:24:13 -0700

    finished static pages

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
+  root 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/help'
-=======
+  get 'static_pages/about'
 
-  resources :users
-
-  root 'users#new'
->>>>>>> modelling-users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
