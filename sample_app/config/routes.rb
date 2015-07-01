1941a9eddf900809029742ac9ff70f071d118a8c
wardch <charlieward18@gmail.com>
2015-06-30 18:11:30 -0700

    finished user signup

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users
+
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
   def setup
       @user = User.new(name: "Example User", email: "user@example.com",
                        password: "foobar", password_confirmation: "foobar")
-    end
+  end
 
  test "name should be present" do
   @user.name = "     "
