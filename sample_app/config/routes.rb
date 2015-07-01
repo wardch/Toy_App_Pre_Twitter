bfce477a4f95764247e6f74eb6683dd517c4ca4b
wardch <charlieward18@gmail.com>
2015-06-30 19:04:06 -0700

    updated session controller, left off at section 8.1.4

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources :users
 
   root 'static_pages#home'
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
