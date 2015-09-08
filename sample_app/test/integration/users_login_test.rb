2921f2c9cb63db5f72931eefee74fd423e1ea05f
wardch <charlieward18@gmail.com>
2015-09-08 09:33:57 -0700

    labour day weekend login integration tests with capybara

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -1,15 +1,21 @@
-require 'test_helper'
-
-class UsersLoginTest < ActionDispatch::IntegrationTest
+# require 'rspec/rails'
+# require 'capybara/rspec'
+# require 'capybara/rails'
+require 'rails_helper'
   # test "the truth" do
-  #   assert true
-  # end
-  test "the flash should display once after the wrong information is supplied for password/email" do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path, session: { password: '', email: ''}
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
+desribe 'the login process', :type => :feature do
+
+  before :each do
+   User.make(:email => 'user@example.com', :password => 'password')
+  end
+
+  it 'should login a user with the corrent information' do
+    visit '/login'
+    within('#create') do
+      fill_in 'email', :with => "user@example.com"
+      fill_in 'password', :with => 'password'
+    end
+    click_button 'Login'
+    expect(page).to have_css('img', text: "#{User.name}")
   end
 end
