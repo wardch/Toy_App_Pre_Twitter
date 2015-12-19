f1d0231656209197e967071469eba503dce5a9b4
wardch <charlieward18@gmail.com>
2015-12-19 12:08:03 -0800

    changes some routes in the routes file

--- sample_app/README.md
+++ sample_app/README.md
@@ -1,1 +1,2 @@
 This is the new updated readme for my sample toy app, I am writing this mesage form the command line!
+chuck dubs
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users
 
-  root 'static_pages#home'
+  root 'application#hello'
 
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
