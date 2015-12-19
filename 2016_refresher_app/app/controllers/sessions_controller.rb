a9c49ee748b8294a5d865165f10d0665a55a7315
wardch <charlieward18@gmail.com>
2015-12-19 13:48:16 -0800

    added all the new files

---
+++ 2016_refresher_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to user
+    else
+      # render a flash message
+      flash.now[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_url 
+  end
+end
