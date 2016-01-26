ad77a61bd1ea91593d8110258421d304b85ae123
wardch <charlieward18@gmail.com>
2016-01-25 22:24:21 -0800

    changed controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,14 +59,6 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in'
-      redirect_to login_url
-    end
-  end
-
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
